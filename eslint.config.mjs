70d11cecdb7f5081908098009f5ed77a4a0202c6
Parth <215315028+nystar1@users.noreply.github.com>
2026-04-08 20:14:20 +0530

    Add YAML linting for locale files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import * as yamlParser from "yaml-eslint-parser";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["**/*.yml", "**/*.yaml"],
+    languageOptions: {
+      parser: yamlParser,
+    },
+    rules: {},
+  },
   globalIgnores([
     ".next/**",
     "out/**",
